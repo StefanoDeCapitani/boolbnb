5a844c2820f54d0dbba65859f5adec9971b272df
andre-cyb <andrea.vg.malfatti@gmail.com>
2022-02-18 16:24:56 +0100

    test init braintree (composer install)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+/* use App\Providers\Braintree_Configuration; */
+/* require_once 'PATH_TO_BRAINTREE/lib/Braintree.php'; */
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        
+        /* \Braintree_Configuration::environment(env('BRAINTREE_ENV'));
+        \Braintree_Configuration::merchantId(env('BRAINTREE_MERCHANT_ID'));
+        \Braintree_Configuration::publicKey(env('BRAINTREE_PUBLIC_KEY'));
+        \Braintree_Configuration::privateKey(env('BRAINTREE_PRIVATE_KEY')); */
     }
 }
