cd2d4f05a62d2676b918c07e7349a60cd18edd17
Stefano De Capitani <stefano.deca@live.com>
2022-02-19 19:34:52 +0100

    Adds markers change on search + maxbounds and zoom

--- resources/js/components/FilterData.vue
+++ resources/js/components/FilterData.vue
@@ -1,104 +1,141 @@
 <template>
     <div>
-        
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-    Filtra
-    </button>
+        <button
+            type="button"
+            class="btn btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+        >
+            Filtra
+        </button>
 
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg modal-fullscreen-sm-down">
-        <div class=" modal-content ">
-        <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Filtri</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            <div>
-                <label for="">numero stanze</label>
-                <button @click="filters.rooms--" >-</button>
-                <input type="text" readonly v-model="filters.rooms">
-                <button @click="filters.rooms++">+</button>
-            </div>
-            <div>
-                <label for="">numero letti</label>
-                <button @click="filters.beds--" >-</button>
-                <input type="text" readonly v-model="filters.beds">
-                <button @click="filters.beds++">+</button>
-            </div>
-            <div>
-                <label for="">numero bagni</label>
-                <button @click="filters.bathrooms--" >-</button>
-                <input type="text" readonly v-model="filters.bathrooms">
-                <button @click="filters.bathrooms++">+</button>
-            </div>
-            <div>
-                <div v-for="service in services" :key="service.id">
-                    <label :for="service.name" >{{ service.name }}</label>
-                    <input :id="service.name" type="checkbox" v-model="filters.activeServices" :value="service.id">
-
+        <!-- Modal -->
+        <div
+            class="modal fade"
+            id="exampleModal"
+            tabindex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+        >
+            <div class="modal-dialog modal-lg modal-fullscreen-sm-down">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">
+                            Filtri
+                        </h5>
+                        <button
+                            type="button"
+                            class="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                        ></button>
+                    </div>
+                    <div class="modal-body">
+                        <div>
+                            <label for="">numero stanze</label>
+                            <button @click="filters.rooms--">-</button>
+                            <input
+                                type="number"
+                                readonly
+                                v-model="filters.rooms"
+                            />
+                            <button @click="filters.rooms++">+</button>
+                        </div>
+                        <div>
+                            <label for="">numero letti</label>
+                            <button @click="filters.beds--">-</button>
+                            <input
+                                type="number"
+                                readonly
+                                v-model="filters.beds"
+                            />
+                            <button @click="filters.beds++">+</button>
+                        </div>
+                        <div>
+                            <label for="">numero bagni</label>
+                            <button @click="filters.bathrooms--">-</button>
+                            <input
+                                type="number"
+                                readonly
+                                v-model="filters.bathrooms"
+                            />
+                            <button @click="filters.bathrooms++">+</button>
+                        </div>
+                        <div>
+                            <div v-for="service in services" :key="service.id">
+                                <label :for="service.name">{{
+                                    service.name
+                                }}</label>
+                                <input
+                                    :id="service.name"
+                                    type="checkbox"
+                                    v-model="filters.activeServices"
+                                    :value="service.id"
+                                />
+                            </div>
+                        </div>
+                        <div>
+                            <label for="">Seleziona raggio di ricerca</label>
+                            <select v-model="filters.range">
+                                <option value="10000">10 km</option>
+                                <option value="20000" selected>20 km</option>
+                                <option value="50000">50 km</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button
+                            type="button"
+                            class="btn btn-secondary"
+                            @click="resetFilters"
+                        >
+                            Reset
+                        </button>
+                        <button
+                            type="button"
+                            class="btn btn-primary"
+                            data-bs-dismiss="modal"
+                            @click="apply()"
+                        >
+                            Applica
+                        </button>
+                    </div>
                 </div>
             </div>
-            <div>
-                <label for="">Seleziona raggio di ricerca</label>
-                <select v-model="filters.range">
-                    <option value="10000">10 km</option>
-                    <option value="20000" selected>20 km</option>
-                    <option value="50000" >50 km</option>
-                </select>
-            </div>
-
         </div>
-        <div class="modal-footer">
-            
-            <button type="button" class="btn btn-secondary"  >Reset</button>
-            <button type="button" class="btn btn-primary" data-bs-dismiss="modal"  @click="apply()" >Applica</button>
-        </div>
-        </div>
-    </div>
-    </div>
-
-    <!-- <div class="modal-dialog modal-fullscreen-sm-down">
-    
-
-
-    </div> -->
     </div>
 </template>
 
 <script>
-    import {services} from '@tomtom-international/web-sdk-services';
-
-    export default {
-        name: 'FilterData',
-        props: {
-            services: Array
+export default {
+    name: "FilterData",
+    props: {
+        services: Array,
+    },
+    data() {
+        return {
+            filters: {
+                rooms: 1,
+                beds: 1,
+                bathrooms: 1,
+                activeServices: [],
+                range: "20000",
+            },
+        };
+    },
+    methods: {
+        apply() {
+            this.$emit("apply-filter", this.filters);
         },
-        data(){
-            return{
-                
-                filters: {
-                    rooms: 1,
-                    beds: 1,
-                    bathrooms: 1,
-                    activeServices: [],
-                    range: "",
-                }
-                
-
-            }
+        resetFilters() {
+            this.filters = {
+                rooms: 1,
+                beds: 1,
+                bathrooms: 1,
+                activeServices: [],
+                range: "20000",
+            };
         },
-        mounted() {
-
-        },
-        methods:{
-
-            apply(){
-
-                
-                this.$emit('apply-filter', this.filters)
-            }
-        }
-
-    }
+    },
+};
 </script>
--- resources/js/components/MyMap.vue
+++ resources/js/components/MyMap.vue
@@ -1,92 +1,63 @@
 <template>
-<div>
-  <h1>Map</h1>
-  <div id="map" style='width: 500px; height: 500px;'>
-      
-  </div>
-</div>
+    <div>
+        <h1>Map</h1>
+        <div id="map" style="width: 500px; height: 500px"></div>
+    </div>
 </template>
 
-
 <script>
-
-import tt from '@tomtom-international/web-sdk-maps';
+import tt from "@tomtom-international/web-sdk-maps";
 
 export default {
-    name: 'MyMap',
-    
-    data(){
-      return{
-        map: null,
-        markers: []
-      }
+    name: "MyMap",
+    data() {
+        return {
+            map: null,
+            markers: [],
+        };
     },
-    props: { 
-      center: Object,
-      flats: Array 
-      },
-    
-
-
-
-    watch:{
-      center(newValue){
-        this.map.flyTo( {center: newValue, zoom: 8})
+    props: {
+        center: Object,
+        flats: Array,
+    },
+    watch: {
+        center(newValue) {
+            this.map.flyTo({ center: newValue, zoom: 8 });
         },
-
-
-       flats(newValue){
-        console.log(newValue)
-        if (newValue) {
-        
-          for (let flat of newValue) {
-              console.log(flat)
-            this.markers.push(new tt.Marker()
-            .setLngLat({lon:flat.lon , lat:flat.lat})
-            .addTo(this.map));
-      
-          }
-          
-        }
-        }   
+        flats(newValue) {
+            for (let marker of this.markers) {
+                marker.remove();
+            }
+            if (newValue) {
+                for (let flat of newValue) {
+                    this.markers.push(
+                        new tt.Marker()
+                            .setLngLat({ lon: flat.lon, lat: flat.lat })
+                            .addTo(this.map)
+                    );
+                }
+            }
+        },
     },
-
-     mounted(){
-
-      this.startMap();
-      
-
+    mounted() {
+        this.startMap();
     },
-    
-
-    methods:{
-      startMap(){
-        this.map = tt.map({
-        key: 'xBR8QUT6VbrPi6uqGXoWGBZbcR4mSfgR',
-        container: 'map',
-        center: this.center,
-        zoom: 10,
-        
-      })
-
-//       if(this.flats.length > 0 ){
-
-//       console.log(this.flats)
-//       new tt.Marker()
-//       .setLngLat([12.497263608239923, 41.90250380717655])
-//       .addTo(map);
-//  }
-      
-      
-
-      //  this.map = Object.freeze(map);
-      }
+    methods: {
+        startMap() {
+            this.map = tt.map({
+                key: "xBR8QUT6VbrPi6uqGXoWGBZbcR4mSfgR",
+                container: "map",
+                center: this.center,
+                zoom: 8,
+                minZoom: 4,
+                maxBounds: [
+                    [5.7499552751, 35.619987291],
+                    [19.4802470232, 48.1153931748],
+                ],
+            });
+        },
     },
-
-   
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
