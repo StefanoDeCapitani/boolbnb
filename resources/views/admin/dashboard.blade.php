4e047821592227fcaaa96b86c7790d2021a63b87
andre-cyb <andrea.vg.malfatti@gmail.com>
2022-02-24 18:00:49 +0100

    modal+style+check new user

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -10,19 +10,14 @@
     @else
         {{Auth::User()->email}}
     @endif
-</h1>  
-<h2 class="text-center my-5">Ecco i tuoi Appartamenti</h2>  
-<a class="btn btn-primary btn_add text-white" href="{{route("admin.flats.create")}}">Aggiungi un appartamento</a>
+</h1>    
 
-    
-    @foreach ($flats as $flat)
+
+    {{-- @foreach ($flats as $flat)
     <div class="row d-flex position-relative mb-5">
-
-        {{-- <div class="col-7"> --}}
-
             <div class="position-absolute col-4" style="transform: translate(0, -50%);top: 50%;left: 0; z-index:2">
                 <a href="{{route("flats.show",$flat->slug)}}">
-                   <img src="{{asset($flat->cover_img)}}" style="width: 60%;border-radius: 13px;" alt="">
+                    <img src="{{asset($flat->cover_img)}}" style="width: 60%;border-radius: 13px;" alt="">
                 </a>
             </div>
             <div class="col-7 flat_container ms-auto d-flex-column justify-content-center align-items-center border postion-relative" >
@@ -31,8 +26,6 @@
                     <h2 style="overflow-wrap: break-word;">{{$flat->title}}</h2>
                 </div>
                 <div class="w-100 d-flex justify-content-center ">
-                {{-- <h5>{{$flat->title}}</h5> --}}
-                {{-- <a href="{{route("flats.show",$flat->slug)}}" class="btn btn-primary">Visualizza</a> --}}
                 <a class=" m-3 btn btn-primary" href="{{route("admin.flats.edit",$flat->slug)}}">Modifica</a>
                 <a class="m-3 btn btn-primary" href="{{route("flats.messages.index",$flat->slug)}}">Messaggi</a>
                 </div>
@@ -44,14 +37,10 @@
                         Appartamento sponsorizzato
                     </div>
                     @endif
-                    
-                    {{-- @dump($flat->activeSponsorships()->first()) --}}
                 </div>
                 <div class="text-center">
                     <a href="{{route('admin.analytics', $flat->slug)}}">Statistiche</a>
                 </div>
-                {{-- <a class="btn btn-success" href="{{route("admin.sponsorships.create",$flat->slug)}}">Sponsorizza</a> --}}
-                
                 <form class="" action="{{route("admin.flats.destroy",$flat->slug)}}" style="position: absolute; top: 10px;right: 10px;" method="post">
                     @csrf
                     @method('delete')
@@ -61,38 +50,95 @@
                     <p class="m-0">Visite: {{ $flat->views->count() }}</p>
                 </div>
             </div>
-        {{-- </div> --}}
-
     </div>
-    
- 
-    @endforeach
+    @endforeach --}}
+    @if(json_encode($flats) === '[]')
 
+        <div class="text-center my-4">
+            <h1>Ops... non hai aggiunto nessun appartamento</h1>
+            <a class="btn btn_add text-white mt-3" href="{{route("admin.flats.create")}}">Aggiungi un appartamento</a>
+        </div>
 
+    @else
+
+    <div class="row justify-content-center mb-4">
+        <div class="col-9">
+            <div class="row">
+                <div class="col-6"><h3 class="text-start d-inline">Ecco i tuoi Appartamenti:</h3></div>
+                <div class="col-6 text-end"><a class="btn btn_add text-white " href="{{route("admin.flats.create")}}">Aggiungi un appartamento</a></div>
+                
+            </div>
+        </div>
+    </div>  
 
     @foreach ($flats as $flat)
-        <div id="flats">
-
-            <div class="card col-lg-9 col-12 mb-3">
-                <div class="row">
+    <div id="flats">
+        
+        <div class="card col-lg-9 col-12 mb-5">
+            <div class="row align-items-center">
                     <div class="col-3">
                         <a href="{{route("flats.show",$flat->slug)}}">
                             <img src="{{asset($flat->cover_img)}}" style="width: 100%;border-radius: 13px;" alt="">
                         </a>
                     </div>
                     <div class="col-9">
-                        <h5 class="mb-4">{{$flat->title}}</h5>
-                        <h6 class="mb-4">creato il</h6>
-                        <div class="card-body">
-                        <p class="card-text">bottoni</p>
+                        <a href="{{route("flats.show",$flat->slug)}}">
+                            <h5 class="mb-4">{{$flat->title}}</h5>
+                        </a>
+                        <p class="mb-1">Creato il {{ $flat->created_at->format('d-m-Y') }}</p>
+                        <div class="mb-2">
+                            @if (!$flat->activeSponsorships()->first())
+                            <span class="pe-3">Dai un boost al tuo annuncio </span><a href="{{ route('admin.sponsorship', $flat->slug) }}"><button class="btn btn_add p_sponsor text-white"><small>Sponsorizza</small> </button> </a>
+                            @else 
+                            <div>
+                                Sponsorizzazione attiva
+                            </div>
+                            @endif
+                            
+                        </div>
+                        
+                        {{-- bottoni --}}
+                        <div class="card-body d-flex justify-content-around mt-5">
+                            <a href="{{route("flats.messages.index",$flat->slug)}}"><button class="btn btn_add p_sponsor btn-message text-white"><small>Messaggi</small> </button></a>
+                            <a href="{{route('admin.analytics', $flat->slug)}}"><button class="btn btn_add p_sponsor btn-analytics text-white"><small>Statistiche</small> </button></a>
+                            <a href="{{route("admin.flats.edit",$flat->slug)}}"><button class="btn btn_add p_sponsor btn-edit"><small>Modifica</small> </button></a>
+
+                            <button type="button" class="btn  btn_add p_sponsor btn-delete text-white" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                Elimina
+                            </button>
+                            
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body text-center">
+                                    <h5>Sei sicuro di voler Eliminare definitivamente l'annuncio?</h5>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn_add p_sponsor btn-analytics text-white" data-bs-dismiss="modal">Close</button>
+                                    <form class="" action="{{route("admin.flats.destroy",$flat->slug)}}" method="post">
+                                        @csrf
+                                        @method('delete')
+                                        <button class="btn btn_add p_sponsor btn-delete text-white">Elimina</button>
+                                    </form>
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+                            
+
+
+                        </div>
                     </div>
-                </div>
-                
                 </div>
             </div>
         </div>
     @endforeach
-
+    
+    @endif
 
     
-@endsection
+    @endsection
