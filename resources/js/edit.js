bb3a5936433136590aa8db4dfb9b7afff4396819
MATTEOTOSINI94 <matteotosini300@icloud.com>
2022-02-25 16:16:17 +0100

    Update input edit

--- resources/js/edit.js
+++ resources/js/edit.js
@@ -3,58 +3,74 @@ const imageCont = document.getElementById('imagePreview')
 const immagine = document.getElementById('immagine')
 const iBox = document.getElementById('i-box')
 
-// const inpMultiple = document.getElementById('file-input')
-// const numOfFiles = document.getElementById('num-of-files')
-// const imageContainer = document.getElementById('images')
-// console.log(iBox)
-
-// inpMultiple.addEventListener('change',function(){
-//    imageContainer.innerHTML= ""
-//     numOfFiles.textContent = `${this.files.length} File Selezionati `
-
-//     for (i of this.files){
-
-
-//         let readerMultiple = new FileReader();
-//         let figure = document.createElement('figure');
-//         let figCap = document.createElement('figcaption');
-//         figCap.innerText = i.name;
-//         figure.appendChild(figCap);
-        
-//         readerMultiple.addEventListener('load',function(){
-//             let img = document.createElement('img');
-//             figure.setAttribute('class','col-12 col-lg-4')
-//             img.setAttribute('src',readerMultiple.result);
-//             figure.insertBefore(img,figCap);
-//         })
-//         imageContainer.appendChild(figure);
-//         readerMultiple.readAsDataURL(i)
-
-        
-        
-//     }
-
-// })
-
+const inpMultiple = document.getElementById('file-input')
+const numOfFiles = document.getElementById('num-of-files')
+const imageContainer = document.getElementById('images')
 const valueInput = JSON.parse(inpFile.dataset.valueinp);
 const ipActual = location.host;
-console.log(ipActual)
+console.log(iBox)
 
-immagine.setAttribute('src', `http://${ipActual}/${valueInput}`)
-immagine.setAttribute('class','d-block')
+const multipleValue = JSON.parse(inpMultiple.dataset.multipleimg)
+console.log(multipleValue);
+
+(function multipleImg(){
+   multipleValue.forEach(path => {
+   
+    const pathImg = path.path
+    console.log(pathImg)
+    numOfFiles.textContent = `${multipleValue.length} File Selezionati `
+    let figure = document.createElement('figure');
+    let figCap = document.createElement('figcaption');
+    let img = document.createElement('img');
+    figure.appendChild(img,figCap);
+    figure.setAttribute('class','col-12 col-lg-4')
+    img.setAttribute('src',`http://${ipActual}/${pathImg}`);
+    imageContainer.appendChild(figure);
+  });
+
+  
 
 
-iBox.addEventListener('click', function(){
-    immagine.setAttribute('src','')
-    immagine.setAttribute('class','d-none')
-    
-  })
+
+})();
+
+inpMultiple.addEventListener('change',function(){
+   imageContainer.innerHTML= ""
+    numOfFiles.textContent = `${this.files.length} File Selezionati `
+
+    for (i of this.files){
+        let readerMultiple = new FileReader();
+        let figure = document.createElement('figure');
+        let figCap = document.createElement('figcaption');
+      
+        figure.appendChild(figCap);
+        readerMultiple.addEventListener('load',function(){
+            let img = document.createElement('img');
+            figure.setAttribute('class','col-12 col-lg-4')
+            img.setAttribute('src',readerMultiple.result);
+            figure.insertBefore(img,figCap);
+        })
+        imageContainer.appendChild(figure);
+        readerMultiple.readAsDataURL(i)
+    }
+
+});
+
+
+(function imagePreview(){  
+  immagine.setAttribute('src', `http://${ipActual}/${valueInput}`)
+  immagine.setAttribute('class','d-block')
+  iBox.addEventListener('click', function(){
+      immagine.setAttribute('src','')
+      immagine.setAttribute('class','d-none')
+    })
+})();
+
+
 
 inpFile.addEventListener('change',function(){
  let file = this.files[0]
- console.log(inpFile.files)
 
- console.log(file)
  if (file) {
      const reader = new FileReader();
      console.log(reader)
