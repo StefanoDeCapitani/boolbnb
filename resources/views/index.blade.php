1a5ad4d22a54df8febb7aa78ed925c45217209cd
yanitejeda <tejedasp@gmail.com>
2022-02-28 18:28:36 +0100

    mod

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,76 +8,107 @@
 
 @section('content')
 
-    <div>
+    <div class="mb-3">
         {{-- search bar --}}
-        
-        
+
+
         <div class="homePage row justify-content-center">
             <h1 class=" text-center">Che il viaggio abbia inizio</h1>
-            <div id="search-box" class=" col-12 col-md-6 " >
+            <div id="search-box" class=" col-12 col-md-6 ">
             </div>
         </div>
         {{-- hero --}}
         <div class="Hero_GuestPage pb-4">
-            
+
         </div>
     </div>
     <div class="container">
-        <h3 class="pb-4  pt-5 text-center">Non sai dove andare?</h3>
         {{-- luoghi consigliati --}}
-        <div class="row justify-content-center">
-            <div class=" col-12 p-2 col-md-6 col-lg-2">
-                <div class=" recommendedPlaces">
-                    <img src="\storage\img\venezia.jpg" alt="">
-                    <h2 class="RecomenText">
-                        Venezia
-                    </h2>
+        <div class="my-5">
+            <h3 class="pb-5  pt-5 text-center">Non sai dove andare?</h3>
+            {{-- luoghi consigliati --}}
+            <div class="row justify-content-center pb-4">
+                <div class=" col-12 p-2 col-md-6 col-lg-2">
+                    <div class=" recommendedPlaces">
+                        <img src="\storage\img\venezia.jpg" alt="">
+                        <h2 class="RecomenText">
+                            Venezia
+                        </h2>
+                    </div>
+                </div>
+                <div class="col-12 p-2 col-md-6 col-lg-2">
+                    <div class=" recommendedPlaces">
+                        <img src="\storage\img\pisa.jpg" alt="">
+                        <h2 class="RecomenText">
+                            Firenze
+                        </h2>
+                    </div>
+                </div>
+                <div class="col-12 p-2 col-md-6 col-lg-2">
+                    <div class=" recommendedPlaces">
+                        <img src="\storage\img\roma.jpg" alt="">
+                        <h2 class="RecomenText">
+                            Roma
+                        </h2>
+                    </div>
+                </div>
+                <div class="col-12 p-2 col-md-6 col-lg-2">
+                    <div class=" recommendedPlaces">
+                        <img src="\storage\img\cagliari.jpg" alt="">
+    
+                        <h2 class="RecomenText">
+                            Cagliari
+                        </h2>
+                    </div>
                 </div>
             </div>
-            <div class="col-12 p-2 col-md-6 col-lg-2">
-                <div class=" recommendedPlaces">
-                    <img src="\storage\img\pisa.jpg" alt="">
-                    <h2 class="RecomenText">
-                        Firenze
-                    </h2>
-                </div>
-            </div>
-            <div class="col-12 p-2 col-md-6 col-lg-2">
-                <div class=" recommendedPlaces">
-                    <img src="\storage\img\roma.jpg" alt="">
-                    <h2 class="RecomenText">
-                        Roma
-                    </h2>
-                </div>
-            </div>
-            <div class="col-12 p-2 col-md-6 col-lg-2">
-                <div class=" recommendedPlaces">
-                    <img src="\storage\img\cagliari.jpg" alt="">
 
-                    <h2 class="RecomenText">
-                        Cagliari
-                    </h2>
-                </div>
+        </div>
+
+        <div class="my-5">
+            {{-- Sezione degli appartamenti in evidenza --}}
+            <h3 class="pb-5  pt-5 text-center">Appartamenti in evidenza</h3>
+            <div class="row justify-content-center">
+                @foreach ($flatsSponsered as $flat)
+                    <div class="col-12 p-2 col-md-6 col-lg-3">
+                        <div class="GuestSponsoredCard" style="width: 18rem; max-height: 188px">
+                            <img src="{{ asset($flat->cover_img) }}" class="card-img-top" style="width: 100%" alt="cover">
+                            <div class="card-body TextCard">
+                                <h4 class="card-title">{{ $flat->title }}</h4>
+    
+                                <a href="{{ route('flats.show', $flat->slug) }}" class="btn btn-primary">Visualizza</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
+
         </div>
 
 
 
-        {{-- Sezione degli appartamenti in evidenza --}}
-        <h5 class="pb-4  pt-5 text-center">Appartamenti in evidenza</h5>
-        <div class="row justify-content-center">
-            @foreach ($flatsSponsered as $flat)
-                <div class="col-12 p-2 col-md-6 col-lg-3">
-                    <div class="GuestSponsoredCard" style="width: 18rem; max-height: 188px">
-                        <img src="{{ asset($flat->cover_img) }}" class="card-img-top" style="width: 100%" alt="cover">
-                        <div class="card-body TextCard">
-                            <h4 class="card-title">{{ $flat->title }}</h4>
+        <div class="my-5">
 
-                            <a href="{{ route('flats.show', $flat->slug) }}" class="btn btn-primary">Visualizza</a>
-                        </div>
-                    </div>
+            <div class="row justify-content-center align-items-center py-5">
+                <div class="col-10 py-5 col-lg-5 chiSiamo">
+                    <h4 class="pb-2"> <strong>Chi siamo </strong></h4>
+                    <p>
+                       <strong> BoolBnB </strong>è nata con l’obiettivo di reinventare il modo di viaggiare dei giovani studenti, freelancer,
+                        docenti del
+                        mondo del web.
+                        Consapevoli di quanto possa essere stressante spostarsi con l'attrezzatura adeguata per l'avorare, Boolbnb ti
+                        aiuta a
+                        trovare la struttura che soddisfi maggiormente i tuoi bisobni, che tu abbia bisogno di un secondo monitor, di una connessione veloce o semplicemete della paperella di boolean, su boolbnb puoi trovare un host con tutto quello di cui hai bisogno.
+                    </p>
                 </div>
-            @endforeach
+                <div class="col-10 my-3 text-center col-lg-5" >
+                    <img style="width: 80% ; border-radius:25px ; text-center"  src="\storage\img\Student.jpg" alt="">
+    
+                </div>
+    
+               
+    
+            </div>
         </div>
     </div>
 
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -7,130 +7,123 @@
 @section('title-page', 'Home page')
 
 @section('content')
-<div class="container py-5 mt-4">
+    <div>
+        {{-- immagine presentazione --}}
+        <div class="text-center">
+            <img class="heroImg" src="{{ asset($flat->cover_img) }}" alt="">
+        </div>
+    </div>
 
-    <div class="row justify-content-center">
-        <div class="col-12 col-lg-10 ">
+    <div class="container pb-5 mt-4">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-10 ">
 
+                {{-- titolo appartamento --}}
+                @if (session('message'))
+                    <div class="alert alert-success">{{ session('message') }}</div>
+                @endif
+                <div class="text-center mb-3 mt-2">
+                    <h1>{{ $flat->title }}</h1>
+                </div>
 
+                {{-- slider / servizzi --}}
+                <div class="row justify-content-center  py-5">
+                    <div class="col-10 my-1 col-lg-6">
+                        <div id="carouselExampleControls" class="carousel slide carousel-fade" data-bs-ride="carousel">
 
-            {{-- titolo appartamento --}}
-            @if (session('message'))
-                <div class="alert alert-success">{{ session('message') }}</div>
-            @endif
-            <div class="text-center mb-3 mt-5">
-                <h1>{{ $flat->title }}</h1>
-            </div>
-            {{-- immagine presentazione --}}
-            <div class="text-center  my-5">
-                <img class="heroImg" src="{{ asset($flat->cover_img) }}" style=" border-radius:25px" alt="">
-            </div>
+                            <div class="carousel-inner">
+                                @foreach ($flat->images as $image)
+                                    <div class="carousel-item @if ($loop->first) active @endif ">
+                                        <img src="{{ asset($image->path) }}" class="d-block w-100  imgCarousel"
+                                            style="height:300px" alt="...">
+                                    </div>
+                                @endforeach
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+                                data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+                                data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
 
+                    </div>
 
+                    <div class="col-10 my-3 col-lg-6 text-center serviceItem py-5">
+                        <h2 class="py-2">Cosa Troverai</h2>
 
-
-
-
-            {{-- slider / servizzi --}}
-            <div class="row justify-content-center  py-5">
-                <div class="col-10 my-1 col-lg-6">
-                    <div id="carouselExampleControls" class="carousel slide carousel-fade" data-bs-ride="carousel">
-
-                        <div class="carousel-inner">
-                            @foreach ($flat->images as $image)
-                                <div class="carousel-item @if ($loop->first) active @endif ">
-                                    <img src="{{ asset($image->path) }}" class="d-block w-100  imgCarousel"
-                                        style="height:300px" alt="...">
-                                </div>
+                        <div class="d-flex justify-content-center px-2 flex-wrap">
+                            @foreach ($flat->services as $service)
+                                <span class="px-2"> <i class="{{ $service->icon }}"></i>
+                                    {{ $service->name }}</span>
                             @endforeach
                         </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
-                            data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
-                            data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button>
-                    </div>
-
-                </div>
-
-                <div class="col-10 my-3 col-lg-6 text-center serviceItem py-5">
-                    <h2 class="py-2">Cosa Troverai</h2>
-
-                    <div class="d-flex justify-content-center px-2 flex-wrap">
-                        @foreach ($flat->services as $service)
-                            <span class="px-2"> <i class="{{ $service->icon }}"></i>
-                                {{ $service->name }}</span>
-                        @endforeach
-                    </div>
-                    <div class="mt-3 d-flex justify-content-center flex-wrap">
-                        <p class="px-2">Numero letti:{{ $flat->n_beds }}</p>
-                        <p class="px-2">Numero bagni: {{ $flat->n_bathrooms }}</p>
-                        <p class="px-2">Numero stanze : {{ $flat->n_rooms }}</p>
-                        <p class="px-2">Metri quadri: {{ $flat->sq_metres }}</p>
+                        <div class="mt-3 d-flex justify-content-center flex-wrap">
+                            <p class="px-2">Numero letti:{{ $flat->n_beds }}</p>
+                            <p class="px-2">Numero bagni: {{ $flat->n_bathrooms }}</p>
+                            <p class="px-2">Numero stanze : {{ $flat->n_rooms }}</p>
+                            <p class="px-2">Metri quadri: {{ $flat->sq_metres }}</p>
+                        </div>
                     </div>
                 </div>
+
+                {{-- messaggi --}}
+                <div class="row d-flex position-relative align-items-center  py-5">
+
+                    <div class="col-12 my-3 justify-content-center col-lg-4 text-center py-5 serviceItem">
+                        <form class="pad-x" action="{{ route('flats.messages.store', $flat->slug) }}"
+                            method="POST">
+                            @csrf
+                            <div class="mb-3">
+                                <h4>Invia Messaggio</h4>
+                                <label for="name" class="form-label">Nome</label>
+                                <input type="text " style=" border-radius: 15px" class="form-control" id="name"
+                                    name="name" value="{{ old('name') }}" style="
+                                        border-radius: 15px">
+                                @error('name')
+                                    {{ $message }}
+                                @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label for="email" class="form-label">E-mail</label>
+                                <input type="text " style=" border-radius: 15px" class="form-control" id="email"
+                                    name="email" value="{{ Auth::user() ? Auth::user()->email : '' }}">
+                                @error('email')
+                                    {{ $message }}
+                                @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label for="text" class="form-label">Messaggio</label>
+                                <textarea class="form-control " style="border-radius: 15px" id="text" name="text"
+                                    rows="6">{{ old('text') }}</textarea>
+                                @error('text')
+                                    {{ $message }}
+                                @enderror
+                            </div>
+                            <button class="btn btn-secondary " style="border-radius: 15px">Invia</button>
+                        </form>
+                    </div>
+
+                    <div class="col-12 my-3 justify-content-center col-lg-7">
+                        <img class="w-100" style="border-radius:25px" src="{{ asset($flat->cover_img) }}"
+                            alt="">
+                    </div>
+                </div>
+
             </div>
 
-            {{-- messaggi --}}
-            <div class="row d-flex position-relative align-items-center  py-5">
+            <div class="row justify-content-center  py-5">
+                <div id="map" style="height: 500px" class="col-10 my-1 col-lg-12" data-lat="{{ $flat->lat }}"
+                    data-lon="{{ $flat->lon }}">
 
-                <div class="col-12 my-3 justify-content-center col-lg-4 text-center py-5 serviceItem">
-                    <form class="pad-x" action="{{ route('flats.messages.store', $flat->slug) }}" method="POST">
-                        @csrf
-                        <div class="mb-3">
-                            <h4>Invia Messaggio</h4>
-                            <label for="name" class="form-label">Nome</label>
-                            <input type="text " style=" border-radius: 15px" class="form-control" id="name" name="name"
-                                value="{{ old('name') }}" style="
-                                    border-radius: 15px">
-                            @error('name')
-                                {{ $message }}
-                            @enderror
-                        </div>
-                        <div class="mb-3">
-                            <label for="email" class="form-label">E-mail</label>
-                            <input type="text " style=" border-radius: 15px" class="form-control" id="email" name="email"
-                                value="{{ Auth::user() ? Auth::user()->email : '' }}">
-                            @error('email')
-                                {{ $message }}
-                            @enderror
-                        </div>
-                        <div class="mb-3">
-                            <label for="text" class="form-label">Messaggio</label>
-                            <textarea class="form-control " style="border-radius: 15px" id="text" name="text"
-                                rows="6">{{ old('text') }}</textarea>
-                            @error('text')
-                                {{ $message }}
-                            @enderror
-                        </div>
-                        <button class="btn btn-secondary " style="border-radius: 15px">Invia</button>
-                    </form>
-                </div>
-
-                <div class="col-12 my-3 justify-content-center col-lg-7">
-                    <img class="w-100" style="border-radius:25px" src="{{ asset($flat->cover_img) }}" alt="">
                 </div>
             </div>
 
         </div>
-
-        <div class="row justify-content-center  py-5">
-            <div id="map" style="height: 500px" class="col-10 my-1 col-lg-12" data-lat="{{ $flat->lat }}"
-                data-lon="{{ $flat->lon }}">
-
-            </div>
-
-
-
-
-        </div>
-
     </div>
-</div>
 
 @endsection
